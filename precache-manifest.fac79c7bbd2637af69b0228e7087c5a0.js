4a4e57618ad91d1b9de95d8191c59f4a5eb528d5
Max <cloheart3034@gmail.com>
2020-05-24 17:03:44 -0500

    Updates

---
+++ precache-manifest.fac79c7bbd2637af69b0228e7087c5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4051e5219efc781a08331e1897096d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9212ea86b15875f643c8",
+    "url": "/static/css/main.ce10c3a9.chunk.css"
+  },
+  {
+    "revision": "eb28d77116e1b011fc47",
+    "url": "/static/js/2.faea67e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.faea67e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9212ea86b15875f643c8",
+    "url": "/static/js/main.fdaf1ded.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  }
+]);
